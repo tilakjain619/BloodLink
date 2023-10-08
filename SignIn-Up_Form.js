c84a468d9d53419cee2b69b84b3a133d92b104b4
Kshitij Mahale <kshitijmahale02@gmail.com>
2023-10-08 15:18:37 +0530

    Added Sign Up form

---
+++ SignIn-Up_Form.js
@@ -0,0 +1,24 @@
+// for Sign In button
+document.querySelector(".signIn-btn").addEventListener("click",function(){
+    document.querySelector(".signIn_Form_section").classList.add("activeSignIn");
+});
+document.querySelector(".signIn_Form_section .close-btn").addEventListener("click",function(){
+    document.querySelector(".signIn_Form_section").classList.remove("activeSignIn");
+});
+
+// for join us as donor button
+document.querySelector(".signUp-btn").addEventListener("click",function(){
+    document.querySelector(".signUp_Form_section").classList.add("activeSignUp");
+});
+document.querySelector(".signUp_Form_section .close-btn").addEventListener("click",function(){
+    document.querySelector(".signUp_Form_section").classList.remove("activeSignUp");
+});
+
+// for Sign Up button
+document.querySelector("#signUp-btn").addEventListener("click",function(){
+    // document.querySelector(".signUp_Form_section").classList.add("activeSignUp");
+    document.querySelector(".signUp_Form_section").classList.add("activeSignUp-bottom_button");
+});
+document.querySelector(".signUp_Form_section .close-btn").addEventListener("click",function(){
+    document.querySelector(".signUp_Form_section").classList.remove("activeSignUp");
+});
--- SignIn_Form.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".signIn-btn").addEventListener("click",function(){
-    document.querySelector(".signIn_Form_section").classList.add("formActive");
-});
-document.querySelector(".signIn_Form_section .close-btn").addEventListener("click",function(){
-    document.querySelector(".signIn_Form_section").classList.remove("formActive");
-});
